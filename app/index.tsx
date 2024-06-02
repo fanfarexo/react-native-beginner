d7c44af476684211e46b66c8d5f4428aaedf08eb
fanfarexo <fanfarexo@gmail.com>
2024-06-02 17:58:13 +0900

    Feat: 위치 기반 날씨 데이터 수집 및 활용 기능 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("screen");
+
+const API_KEY = `8fc7f6b2b1858b874afc384f5191ee8b`;
 
 export default function Index() {
   const [ok, setOk] = useState(true);
   const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState<any[]>([]);
 
-  const getLocation = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -21,10 +31,15 @@ export default function Index() {
       { useGoogleMaps: false }
     );
     setCity(String(location[0].city));
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.list);
   };
 
   useEffect(() => {
-    getLocation();
+    getWeather();
   }, []);
 
   return (
@@ -38,22 +53,19 @@ export default function Index() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27º</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27º</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27º</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27º</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator size="large" color="white" />
+          </View>
+        ) : (
+          days.map((day) => (
+            <View key={day.dt} style={styles.day}>
+              <Text style={styles.temp}>{day.main.temp.toFixed(1)}º</Text>
+              <Text style={styles.desc}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -89,6 +101,12 @@ const styles = StyleSheet.create({
   desc: {
     marginTop: -10,
     fontSize: 60,
+    fontWeight: "500",
+    color: "white",
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
     fontWeight: "500",
     color: "white",
   },

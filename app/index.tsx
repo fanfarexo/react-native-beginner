709b6c8b998e97f09a08bb444756a72d03d297ef
fanfarexo <fanfarexo@gmail.com>
2024-06-02 16:44:27 +0900

    Feat: 레이아웃 컴포넌트 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,53 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>SEOUL</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27º</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: "600",
+    color: "white",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+    fontWeight: "600",
+    color: "white",
+  },
+  desc: {
+    marginTop: -10,
+    fontSize: 60,
+    fontWeight: "500",
+    color: "white",
+  },
+});
